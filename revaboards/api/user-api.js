f457ee19e6c8fd493a2261d7fa2266a11a754b0b
Wezley <wezley.singleton@gmail.com>
2020-04-09 12:56:46 -0400

    added validation to ensure that a user was grabbed

--- revaboards/api/user-api.js
+++ revaboards/api/user-api.js
@@ -25,11 +25,20 @@ const getUserByCredentials = (un, pw, cb) => {
         // validation to ensure we do not waste resources
         if (!un || !pw) throw Error('Oh no! You gave me bad data'); // truthy/falsy in use here
 
-        // fetch the sought user
-        const user = userData.filter(user => {
-            return user.username === un && user.password == pw;
-        }).pop();
+        // fetch the sought user (declarative-style logic)
+        const user = userData.filter(user => user.username === un && user.password == pw).pop();
 
+        /* 
+            other "functional" methods for arrays include: 
+                - filter
+                - map
+                - reduce
+        */
+
+        // validate that we actually obtained a user
+        if (!user) throw new Error('Invalid credentials provided!');
+
+        // invoke the provided callback function
         cb(user);
 
     }, 250);
