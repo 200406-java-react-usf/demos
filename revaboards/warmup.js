25739ecd2866ac57ad13b661bf48d692c10c4baf
kosema047 <kosema047@gmail.com>
2020-04-09 06:57:35 -0700

    status check on command, some reason missing committed

---
+++ revaboards/warmup.js
@@ -0,0 +1,24 @@
+//single line comment
+
+/*
+    multi-line
+    comment
+*/
+
+/**
+ * Documentation comments (used to indicate official docs)
+ */
+
+ function myFunction() {
+     console.log('this is a function declaration');
+ }
+
+ myFunction();
+ 
+ let yourFunction = function() {
+     console.log('this is a anonymous function, and can be assigned to a variable');
+ }
+
+ yourFunction();
+
+ 
