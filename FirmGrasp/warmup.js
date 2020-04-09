34836c5da872278b1b2de0f1ca43d8cad7991c77
CDPGIT <63257733+CDPGIT@users.noreply.github.com>
2020-04-09 13:19:24 -0400

    commit then remove

---
+++ FirmGrasp/warmup.js
@@ -0,0 +1,22 @@
+// comment
+ /* multi line comment
+ */
+
+ /** 
+* Documentation comments( used to indicate offical docs) 
+*/ 
+
+function myFunction(){ 
+    console.log('this is a function declaration'); 
+    } 
+    
+    myFunction(); 
+    
+    
+    let yourFunction = function(){
+    console.log('This is an anonymous function and can be assigned to a varibale.'); 
+    } 
+    
+    yourFunction();
+
+    console.log(yourFunction);
