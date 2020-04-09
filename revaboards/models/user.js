2bcaa7b21ebd01e8b836308bdb95faecd513ddbb
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-09 12:30:21 -0400

    init project and setup

---
+++ revaboards/models/user.js
@@ -0,0 +1,19 @@
+//module.exports make this function available outside of this file(can be imported)
+module.exports = function User(id ,un, pw, fn, ln, email, dob) {
+    
+    this.id = id;
+    this.username = un;
+    this.password = pw;
+    this.firstname = fn;
+    this.lastname = ln;
+    this.email = email;
+    this.dateofbirth = dob;
+
+    this.age = () => {
+        let ageMs = Date.now() - dob.getTime();
+        let ageDate = new Date(ageMs);
+        let ageYrs = Math.abs(ageDate.getUTCFullYear - 1970);
+        return ageYrs;
+    }
+}
+
