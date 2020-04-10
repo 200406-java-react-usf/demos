fd969e8788fc6cf9268d88649c6c01cab0f9ecee
wandeltk <wandeltk@gmail.com>
2020-04-10 08:44:45 -0600

    trying to commit so I can pull

--- revaboards/index.js
+++ revaboards/index.js
@@ -1,6 +1,9 @@
 const userApi = require('./api/user-api');
-
+const postApi = require('./api/post-api');
 // when you fetch a user by id, also, grab their posts and add them to the user obj
-userApi.getUserById(1, user => {
-    console.log(user);
+userApi.getUserById(1, users => {
+    postApi.getPostsByPosterId(user.id, posts => {
+        user.posts = posts;
+        console.log(users);
+    });
 });
--- revaboards/models/post.js
+++ revaboards/models/post.js
@@ -1,1 +1,9 @@
 // a Post should include the fields: id, title, body, posterId
+module.exports = function Post(id, title, body, posterId) {
+
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.posterId = posterId;
+
+}
