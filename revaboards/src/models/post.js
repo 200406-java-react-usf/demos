a9e35a35fe18d6701e244c996fb93c3a243cc388
Wezley Singleton <wezley.singleton@gmail.com>
2020-04-15 23:22:45 -0400

    transferred revaboards to its own repo

--- revaboards/src/models/post.js
+++
@@ -1,8 +0,0 @@
-// a Post should include the fields: id, title, body, posterId
-module.exports = function Post(id, title, body, posterId) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-    this.posterId = posterId;
-    this.postedTime = new Date();
-}
