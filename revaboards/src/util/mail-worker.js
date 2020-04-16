12df82802cbff1c0db5aa79601e838cbec829b4b
neftaly polanco <polanconeftaly@gmail.com>
2020-04-16 14:52:18 -0400

    added mail-worker and created file for notes on callbacks

--- revaboards/src/util/mail-worker.js
+++ revaboards/src/util/mail-worker.js
@@ -1,3 +1,4 @@
+const events = require('events')
 /**
  * 1. import/require the NodeJS event module so you can access the EventEmitter object/class
  * 
@@ -9,8 +10,21 @@
  * 3. Add an event to MailWorker called 'newRegister', when this event is emitted execute a function that "sends" an email
  *    to the newly registered users email addres"
  *   - console.log() a phrase out that says who you are sending it to and what server/port your are using
-<<<<<<< HEAD
  * */
-=======
- */
->>>>>>> 34a60a0531b752f536ee4bf1e4923c3a73a41182
+function MailWorker() {
+    this.server = 'fake-smtp-server.com';
+    this.port = 25;
+}
+
+MailWorker.prototype.__proto__ = events.EventEmitter.prototype;
+
+const mailWorker = new MailWorker();
+
+mailWorker.on('newRegister', function(recipientEmail) {
+    console.log(`Sending a welcome email to ${recipientEmail} using SMTP server at ${this.server} on port ${this.port}`);
+
+    //pretend to send a message
+    setTimeout(() => {
+        console.log('Message successfully sent!');
+    })
+})
