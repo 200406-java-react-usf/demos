1b12d08ced35969122911830d67b2cdb562c1353
KaneishaR <kaneisha.reese@gmail.com>
2020-04-17 10:10:22 -0500

    update

--- async-patterns/src/exercise.js
+++ async-patterns/src/exercise.js
@@ -1,14 +1,55 @@
 let requestFileFromServer = require('./request-file-from-server');
 
+//hint: use a global var for something
+let array = ['', '',''];
+
+let counter = 0;
+
 function getFile(file) {
     
-    console.log("Requesting: " + file);
+    console.log("Requesting: " + file);    
 
 	requestFileFromServer(file, function(text) {
-		// your implementation here
+
+        if(text == 'This is file 1! It should print first.'){
+            array[0] = text;
+        }
+        
+        if (text == 'This is file 2! It should print second.'){
+            array[1] = text;
+        }
+
+        if (text == 'This is file 3! It should print last.'){
+            array[2] = text;
+        }
+        if(array[0]){
+            console.log(array[0])
+            if(array[1]){
+                console.log(array[1])
+                if (array[3]){
+                    console.log(array[3])
+                }
+            }
+        }
+        counter++;
+
+        // if (counter == 3){
+        //     printFiles();
+        //     console.log('complete!')
+        // }
+
     });
-    
+
+   
 }
+
+// function printFiles(){
+//     for (let i = 0; i < array.length; i++){
+//         console.log(array[i]);
+//     }
+// }
+
+
 
 // FREEBIE: request all files at once in "parallel"
 getFile("file1");
