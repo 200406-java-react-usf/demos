39c8d6533d222adfe7cabd923e3e81eab4a193a1
mohamedaymantaha <maa16f@my.fsu.edu>
2020-04-17 10:12:56 -0500

    my trials of excercise.js

--- async-patterns/src/exercise.js
+++ async-patterns/src/exercise.js
@@ -5,12 +5,59 @@ function getFile(file) {
     console.log("Requesting: " + file);
 
 	requestFileFromServer(file, function(text) {
-		// your implementation here
-    });
+        // your implementation here
+        /*const fl = require(file)
+        fl.readFile(file, (err, data) => {
+            if (err) throw err
+            console.log(data)
+        })*/
+
+
+
+        // Promise
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log(text)
+                resolve()
+                },
+            )
+        
+    })
     
+});
+    console.log("Complete!")
 }
+/*
+function fileRecieved(file, text) {
+
+}
+let responses = {};
+*/
+/*
+function getAll(){
+    getFile("file1")
+    .then(() => {
+        return getFile("file2")
+    })
+    .then (() =>{
+        return getFile("file3")
+    })
+    
+}*/
+/*
+function getAll() {
+    getFile("file1", () => {
+        getFile("file2", () => {
+            getFile("file3", () => {
+            })
+        })
+    })
+console.log("Complete!");
+}*/
 
 // FREEBIE: request all files at once in "parallel"
-getFile("file1");
+getFile("file1")
 getFile("file2");
 getFile("file3");
+
+//getAll;
