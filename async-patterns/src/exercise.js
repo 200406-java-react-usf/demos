7ca12b1a623c97d07cb976f8f76bd125075230b9
HaoTran <31514669+donxiya@users.noreply.github.com>
2020-04-17 10:09:51 -0500

    discard

--- async-patterns/src/exercise.js
+++ async-patterns/src/exercise.js
@@ -1,13 +1,15 @@
 let requestFileFromServer = require('./request-file-from-server');
-
+let recieved = [0, 0, 0]
 function getFile(file) {
     
     console.log("Requesting: " + file);
 
-	requestFileFromServer(file, function(text) {
-		// your implementation here
+    requestFileFromServer(file, function (text) {
+        // your implementation here
+
+
+        
     });
-    
 }
 
 // FREEBIE: request all files at once in "parallel"
@@ -15,4 +17,3 @@ getFile("file1");
 getFile("file2");
 getFile("file3");
 
-const 
