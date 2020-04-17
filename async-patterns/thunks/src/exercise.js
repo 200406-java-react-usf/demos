4bd568cf6714284732e9fd455e6a6f87e0103b7b
KaneishaR <kaneisha.reese@gmail.com>
2020-04-17 11:39:15 -0500

    Thunk exercise

--- async-patterns/thunks/src/exercise.js
+++ async-patterns/thunks/src/exercise.js
@@ -1,16 +1,30 @@
 const requestFileFromServer = require('./request-file-from-server');
 
-function retrieveFile(file) {
+function retrieveFile(file, cb) {
     
     console.log(`Requesting: ${file}`);
 
-    // your thunk implementation here
-    
+    requestFileFromServer(file, function(text){
+        cb(text);
+    });
+
 }
 
+
+
 // FREEBIE: request all files in "parallel" here
-let thunk1 = retrieveFile('file1');
-let thunk2 = retrieveFile('file2');
-let thunk3 = retrieveFile('file3');
+let thunk1 = function(cb) {return (retrieveFile('file1', cb))};
+let thunk2 = function(cb) {return (retrieveFile('file2', cb))};
+let thunk3 = function(cb) {return (retrieveFile('file3', cb))};
 
 // Hmmmm, what to do down here?
+
+thunk1(text => { 
+    console.log(text)
+    thunk2(text => { 
+        console.log(text)
+        thunk3(text => { console.log(text)});
+    }); 
+});
+
+
