8cdfdb63a5b29151c63881e176f409d74686384d
jdvalencia <jdvalencia@users.noreply.github.com>
2020-04-17 11:58:27 -0400

    finished the exercise

--- async-patterns/thunks/src/exercise.js
+++ async-patterns/thunks/src/exercise.js
@@ -3,8 +3,21 @@ const requestFileFromServer = require('./request-file-from-server');
 function retrieveFile(file) {
     
     console.log(`Requesting: ${file}`);
-
     // your thunk implementation here
+    let fn, result;
+    requestFileFromServer(file, (text) =>{
+        result = text;
+         if(fn){
+             fn(result);
+         }
+    })
+
+    return function(cb){
+        if(result){
+            cb(result);     
+        }
+        fn = cb;
+    }
     
 }
 
@@ -14,3 +27,14 @@ let thunk2 = retrieveFile('file2');
 let thunk3 = retrieveFile('file3');
 
 // Hmmmm, what to do down here?
+
+thunk1((result) => {
+    console.log(result);
+    thunk2((result) => {
+        console.log(result);
+        thunk3((result) => {
+            console.log(result);
+            console.log('Complete.');
+        })
+    })
+})
