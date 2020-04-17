942e4cf978358edbb2d8a494810bccf0c1137571
Wezley Singleton <wezley.singleton@gmail.com>
2020-04-16 23:08:42 -0400

    provided more template to thunk exercise

--- async-patterns/thunks/src/exercise.js
+++ async-patterns/thunks/src/exercise.js
@@ -4,4 +4,9 @@ function retrieveFile(file) {
     // your thunk implementation here
 }
 
-// request all files in "parallel" here
+// FREEBIE: request all files in "parallel" here
+let thunk1 = retrieveFile('file1');
+let thunk2 = retrieveFile('file2');
+let thunk3 = retrieveFile('file3');
+
+// Hmmmm, what to do down here?
