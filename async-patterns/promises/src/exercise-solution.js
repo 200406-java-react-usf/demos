b85abc9dcf99622773ccf7d594f193aa2d81af64
Wezley Singleton <wezley.singleton@gmail.com>
2020-04-18 17:07:01 -0400

    added non-fixes file and a clean async impl with error handling

--- async-patterns/promises/src/exercise-solution.js
+++ async-patterns/promises/src/exercise-solution.js
@@ -4,104 +4,40 @@ function retrieveFile(file) {
     
     console.log(`Requesting: ${file}`);
 
-    // Wezley's implementation
-    return new Promise(resolve => {
-        requestFileFromServer(file, resolve);
+    return new Promise((resolve, reject) => {
+
+        requestFileFromServer(file, text => {
+            if(text) resolve(`Request for ${file} success! Text: ${text}`);
+            else reject(`Request for ${file} failed!`);
+        });
+
+    });
+    
+}
+
+//------------------------------------------------------------------
+
+// clean async/await implementation with proper error handling (attr: Alec Batson)
+
+async function multiRetrieve(...files) {
+
+    let getOne = async (file) => {
+        try {
+            return await retrieveFile(file);
+         } catch (e) {
+            return e
+        }
+    }
+    let res = files.map((ele)=>{
+        return getOne(ele)
     });
 
-    // Korey and team's implementation
-    // return new Promise((resolve, reject) => {
-
-    //     requestFileFromServer(file, text => {
-    //         if(text) {
-    //             console.log(`${file} - text is truthy`);
-    //             resolve(text);
-    //         }
-    //         else {
-    //             console.log('text is falsy');
-    //             reject('Request Failed!');
-    //         }
-    //     });
-
-    // });
-}
-
-// FREEBIE: request all files in "parallel" here
-// let promise1 = retrieveFile('file1').catch(reason => console.log(reason));
-// let promise2 = retrieveFile('file2').catch(reason => console.log(reason));
-// let promise3 = retrieveFile('file3').catch(reason => console.log(reason));
-// let promise4 = retrieveFile('file4').catch(reason => console.log(reason));
-
-// Wezley's implementation
-// promise1.then(text => console.log(text))
-//         .then(() => promise2)
-//         .then(text => console.log(text))
-//         .then(() => promise3)
-//         .then(text => console.log(text))
-//         .finally(() => console.log('Complete!'));
-
-//------------------------------------------------------------------
-
-// Alternative Solution using .map and .reduce to manage flow control
-
-// function getFiles(...files) {
-
-//     files.map(retrieveFile) // returns a new array of Promises
-
-//         .reduce((chain, filePromise) => {
-
-//             return chain.then(() => filePromise)
-//                         .then(text => console.log(text));
-
-//         }, Promise.resolve())
-
-//         .finally(() => console.log('Complete!'));
-
-// }
-
-// getFiles('file1', 'file2', 'file3');
-
-//------------------------------------------------------------------
-
-// Alternative Solution using async/await 
-
-async function getFiles() {
-    let filePromises = [retrieveFile('file1'), retrieveFile('file2'), retrieveFile('file3')]
-    for (file of filePromises) {
-        console.log(await file);
+    for(let i =0; i < res.length; i++){
+        console.log(await res[i]);
     }
+
     console.log('Complete!');
+    
 }
 
-getFiles();
-
-//------------------------------------------------------------------
-
-// Korey and team's implementation
-// promise1.then(text=>{
-//     console.log(text);
-//     return promise2;
-// }).then(text=>{
-//     console.log(text);
-//     return promise3;
-// }).then(text=>{
-//     console.log(text);
-//     return promise4;
-// }).then((text)=>{
-//     console.log('Should not print'); 
-// }).finally(()=>{
-//     console.log('Complete!');
-// });
-
-//---------------------------------------------------------
-
-// Non-Fixes (feel free to refactor to make some of these work)
-
-
-// Promise.all --- Does not really solve this problem, since it waits for all files to be retrieved before printing their text
-// Promise.all([retrieveFile('file1'), retrieveFile('file2'), retrieveFile('file3')])
-//        .then(values => {
-//            values.forEach(val => console.log(val));
-//            return values;
-//        })
-//        .finally(() => console.log('Complete!'));
+multiRetrieve('file1', 'file2', 'file3', 'file4');
---
+++ async-patterns/promises/src/non-fixes.js
@@ -0,0 +1,143 @@
+const requestFileFromServer = require('./request-file-from-server');
+
+function retrieveFile(file) {
+    
+    console.log(`Requesting: ${file}`);
+    
+    return new Promise((resolve, reject) => {
+
+        requestFileFromServer(file, text => {
+            if(text) resolve(`Request for ${file} success! Text: ${text}`);
+            else reject(`Request for ${file} failed!`);
+        });
+
+    });
+    
+}
+
+//-------------------------------------------------------------------------------
+
+// First implementation using Promises (meets minimum reqs, but does not handle errors well)
+
+
+// promise1.then(text => {
+//     console.log(text);
+//     return promise2;
+// }).then(text => {
+//     console.log(text);
+//     return promise3;
+// }).then(text => {
+//     console.log(text);
+//     return promise4;
+// }).then(text => {
+//     console.log(`${text} - should not print`);
+// }).catch(err => {
+//     console.log(err);
+// }).finally(() => {
+//     console.log('Complete!');
+// });
+
+//--------------------------------------------------------------------------------
+
+// Second implementation using Promises (meets minimum reqs, but does not handle errors well)
+
+// let promise1 = retrieveFile('file1');
+// let promise2 = retrieveFile('file2');
+// let promise3 = retrieveFile('file3');
+// let promise4 = retrieveFile('file4');
+
+// promise1.then(text => console.log(text))
+//         .then(() => promise2)
+//         .then(text => console.log(text))
+//         .then(() => promise3)
+//         .then(text => console.log(text))
+//         .then(() => promise4)
+//         .then(text => console.log(text))
+//         .catch(err => console.log(err))
+//         .finally(() => console.log('Complete!'));
+
+//--------------------------------------------------------------------------------
+
+// Using .map and .reduce to manage flow control (meets minimum reqs, but does not handle errors well)
+
+// function getFiles(...files) {
+
+//     files.map(retrieveFile) // returns a new array of Promises
+//         .reduce((chain, filePromise) => {
+
+//             return chain.then(() => filePromise)
+//                         .then(text => console.log(text))
+//                         .catch(err => console.log(err));
+
+//         }, Promise.resolve())
+//         .finally(() => console.log('Complete!'));
+
+// }
+
+// getFiles('file1', 'file2', 'file3', 'file4');
+
+
+//-------------------------------------------------------------------------------
+
+// Using async/await (meets minimum reqs, but does not handle errors well)
+
+// async function getFiles(...files) {
+//     try {
+//         let filePromises = files.map(retrieveFile);
+//         for (file of filePromises) {
+//             console.log(await file);
+//         }
+//     } catch (err) {
+//         console.log(err);
+//     } finally {
+//         console.log('Complete!');
+//     }
+// }
+
+// getFiles('file1', 'file2', 'file3', 'file4');
+
+//-------------------------------------------------------------------------------
+
+// Promise.all -- waits for all of the Promises in a provided array to be fulfilled and returns a Promise
+//                containing an array of their resolved values
+
+// Does not really solve this problem, since it waits for all files to be retrieved before printing their text
+// Does not work reliably if errors occur. 
+
+// Promise.all([retrieveFile('file1'), retrieveFile('file2'), retrieveFile('file3'), retrieveFile('file4')])
+//        .then(values => {
+//             values.forEach(val => {
+//                 console.log(val);
+//             });
+//        })
+//        .finally(() => console.log('Complete!'));
+
+//-------------------------------------------------------------------------------
+
+// Promise.allSettled -- waits for all of the Promises in a provided array to be resolved (regardless of fate) and returns a Promise
+//                       containing an array of objects indicating a status of "fulfilled" or "rejected" along with a "value" or "reason"
+//                       property.
+
+// Does not really solve this problem, since it waits for all files to be retrieved before printing their text
+// Handles errors well though!
+Promise.allSettled([retrieveFile('file1'), retrieveFile('file2'), retrieveFile('file3'), retrieveFile('file4')])
+       .then(values => {
+           
+            values.forEach(val => {
+                if (val.status === 'fulfilled') {
+                    console.log(val.value)
+                } else {
+                    console.log(val.reason);
+                }
+            });
+       })
+       .finally(() => console.log('Complete!'));
+
+
+//-------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------
