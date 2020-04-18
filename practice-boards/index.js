9264423586450ee075e0a8ffa9a36ec77146c9e5
Wagenheim <wagenheimk@me.com>
2020-04-18 17:40:10 -0400

    added getUserById with promises

--- practice-boards/index.js
+++ practice-boards/index.js
@@ -2,7 +2,11 @@ const userApi = require('./user-api');
 
 
 //returns array of all users
-userApi.promiseAllUsers.then(result => console.log(result))
+// userApi.promiseAllUsers.then(result => console.log(result))
+//                         .catch(err => console.log(err));
+
+//returns user when given an ID
+userApi.getUserById(3).then(user => console.log(user))
                         .catch(err => console.log(err));
                         
 
--- practice-boards/user-api.js
+++ practice-boards/user-api.js
@@ -4,25 +4,35 @@ const User = require('./user');
 let promiseAllUsers = new Promise((resolve,reject) => {
     setTimeout(() => {
         let users = [];
-        
-        
         for (user of userData){
             users.push(user);
         }
-
         if(users.length <= 0){
             reject('No Users Exists');
             return;
         }
-
         resolve(users);
-        
     }, 1000);
 });
+
+let getUserById = function(id){
+
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            if(!id || id <= 0 || id > userData.length){
+                reject('Please give valid ID');
+            }
+            resolve(userData[id-1]);
+        },1000);
+
+    });
+
+};
 
 
 module.exports = {
 
-    promiseAllUsers
+    promiseAllUsers,
+    getUserById
 
 }
