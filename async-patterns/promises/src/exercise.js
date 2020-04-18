3bff238659ce99f7d90d9eb96e492befc558a982
HaoTran <31514669+donxiya@users.noreply.github.com>
2020-04-17 21:30:54 -0500

    commented

--- async-patterns/promises/src/exercise.js
+++ async-patterns/promises/src/exercise.js
@@ -1,36 +1,36 @@
 const requestFileFromServer = require('./request-file-from-server');
 
-function retrieveFile(file) {
+// function retrieveFile(file) {
 
-    console.log(`Requesting: ${file}`);
+//     console.log(`Requesting: ${file}`);
 
-    // your promise implementation here
-    let newPromise = new Promise(
-        function (resolve) {
-            requestFileFromServer(
-                file, text => {
-                    resolve(text);
-                }
-            )
-        }
-    )
-    return newPromise
-}
+//     // your promise implementation here
+//     let newPromise = new Promise(
+//         function (resolve) {
+//             requestFileFromServer(
+//                 file, text => {
+//                     resolve(text);
+//                 }
+//             )
+//         }
+//     )
+//     return newPromise
+// }
 
-// FREEBIE: request all files in "parallel" here
-let promise1 = retrieveFile('file1');
-let promise2 = retrieveFile('file2');
-let promise3 = retrieveFile('file3');
+// // FREEBIE: request all files in "parallel" here
+// let promise1 = retrieveFile('file1');
+// let promise2 = retrieveFile('file2');
+// let promise3 = retrieveFile('file3');
 
-// Hmmmm, what to do down here?
-promise1.then(function (text) {
-    console.log(text)
-    return promise2;
-}).then(function (text) {
-    console.log(text)
-    return promise3;
-}).then(function (text) {
-    console.log(text)
-    console.log("completed")
-})
+// // Hmmmm, what to do down here?
+// promise1.then(function (text) {
+//     console.log(text)
+//     return promise2;
+// }).then(function (text) {
+//     console.log(text)
+//     return promise3;
+// }).then(function (text) {
+//     console.log(text)
+//     console.log("completed")
+// })
 
