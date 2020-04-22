89f8292ad9272f694e34fe477e5c42298017ec79
neftaly polanco <polanconeftaly@gmail.com>
2020-04-22 15:54:41 -0400

    solved promiises exercise

--- revaboards/neftaly_polanco_code/async-patterns/promises/src/exercise.js
+++ revaboards/neftaly_polanco_code/async-patterns/promises/src/exercise.js
@@ -1,10 +1,26 @@
-const requestFileFromServer = require('./request-file-from-server');
+/**
+ * Solving retrieve file execise using promises
+ * @param {argument} file represents the incomming file from the server
+ * @param {function} cb is a callback when file is retrieved;
+ */
 
+const requestFileFromServer = require('./request-file-from-server');
+let result;
 function retrieveFile(file) {
     
     console.log(`Requesting: ${file}`);
 
     // your promise implementation here
+
+    let promise = new Promise(function(resolve, reject) {
+        requestFileFromServer(file, (text) => {        
+               if(text)
+               resolve(text);                        
+               reject('Request Failed!');           
+        });     
+    });
+    //promise.then(function)
+    return promise;
 
 }
 
@@ -13,4 +29,17 @@ let promise1 = retrieveFile('file1');
 let promise2 = retrieveFile('file2');
 let promise3 = retrieveFile('file3');
 
+
 // Hmmmm, what to do down here?
+promise1.then(text1 => {
+    console.log(text1);
+    return promise2;
+}).then(text2 => {
+    console.log(text2);
+    return promise3;
+}).then(text3 => {
+    console.log(text3);
+}).finally(() => {
+    console.log('Complete!')
+})
+
