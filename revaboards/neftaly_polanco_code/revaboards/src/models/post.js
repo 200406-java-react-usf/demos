f4e95f16386caac9531737213dcd0205a0f96c09
neftaly polanco <polanconeftaly@gmail.com>
2020-04-23 12:32:15 -0400

    change post to typescript

--- revaboards/neftaly_polanco_code/revaboards/src/models/post.js
+++
@@ -1,9 +0,0 @@
-  
-// a post should include the fields: id, title, body, posterId
-module.exports = function Post(id, title, body, posterID, time) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-    this.posterID = posterID;
-    this.time = time;
-}
