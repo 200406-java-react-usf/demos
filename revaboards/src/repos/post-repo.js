054da9b9a6a12bb33ffc647df6af0b0b00785a72
KaneishaR <kaneisha.reese@gmail.com>
2020-04-23 08:54:07 -0500

    pocs

--- revaboards/src/repos/post-repo.js
+++ revaboards/src/repos/post-repo.js
@@ -1,32 +1,44 @@
 // implement and export the methods: getPostById and getPostsByPosterId
 const postData = require('../data/post-db');
+module.exports = (function(){
+    
+    let instance;
+    
+    function init() {
+        const getPostById = (id, cb) => {
+            setTimeout(() => {
+                if (!id || id <= 0) throw new Error('Oh no! You gave me bad data!');
+                const post = postData.filter(post => post.id === id).pop();
+                if (!post) throw new Error('No post found with provided id!');
+                cb(post);
+            }, 250);
+        }
 
-const getPostById = (id, cb) => {
-    setTimeout(() => {
-        if (!id || id <= 0) throw new Error('Oh no! You gave me bad data!');
-        const post = postData.filter(post => post.id === id).pop();
-        if (!post) throw new Error('No post found with provided id!');
-        cb(post);
-    }, 250);
-}
+        const getPostsByPosterId = (posterId, cb) => {
+            setTimeout(() => {
+                const posts = postData.filter(post => post.posterId === posterId);
+                cb(posts);
+            }, 250);
+        }
 
-const getPostsByPosterId = (posterId, cb) => {
-    setTimeout(() => {
-        const posts = postData.filter(post => post.posterId === posterId);
-        cb(posts);
-    }, 250);
-}
+        const addPost = function(title, body, posterId){
+            console.log(postData.length);
+            let id = null;
+            id = postData.length +1;
+            postData.push(new Post(id, title, body, posterId));
+            console.log(postData);
+        }
 
-const addPost = function(title, body, posterId){
-    console.log(postData.length);
-    let id = null;
-    id = postData.length +1;
-    postData.push(new Post(id, title, body, posterId));
-    console.log(postData);
-}
+        return {
+            getPostById,
+            getPostsByPosterId,
+            addPost
+        };
+    }
 
-module.exports = {
-    getPostById,
-    getPostsByPosterId,
-    addPost
-}
+    return{
+        getInstance: function() {
+            return !instance ? instance = init() : instance;
+        }
+    }
+})
