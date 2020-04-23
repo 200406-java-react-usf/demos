98bc16136f620019d8c804533d208dd8f1908497
neftaly polanco <polanconeftaly@gmail.com>
2020-04-23 12:41:37 -0400

    fixed an error in User

--- revaboards/neftaly_polanco_code/revaboards/src/models/user.ts
+++ revaboards/neftaly_polanco_code/revaboards/src/models/user.ts
@@ -1,15 +1,22 @@
-export class Post {
+export class User {
 
 	id: number;
-	title: string;
-	body: string;
-	posterId: number;
+	username: string;
+	password: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+	dob: Date;
+	age: number;
 
-	constructor (id: number, title: string, body: string, pid: number) {
+	constructor(id: number, un: string, pw: string, fn: string, ln: string, email: string, dob: Date) {
 		this.id = id;
-		this.title = title;
-		this.body = body;
-		this.posterId = pid;
+		this.username = un;
+		this.password = pw;
+		this.firstName = fn;
+		this.lastName = ln;
+		this.email = email;
+		this.dob = dob;  
+		this.age = Math.abs(new Date(Date.now() - this.dob.getTime()).getUTCFullYear() - 1970);    
 	}
-	
 }
