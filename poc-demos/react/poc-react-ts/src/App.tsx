ffe7271da66dfcacb5faa0c38679a66c70748f90
donxiya <31514669+donxiya@users.noreply.github.com>
2020-05-13 14:13:37 -0500

    register

--- poc-demos/react/poc-react-ts/src/App.tsx
+++ poc-demos/react/poc-react-ts/src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LoginComponent from './components/LoginComponent';
 import HomeComponent from './components/HomeComponent';
 
 import { User } from './models/user';
+import NavbarComponent from './components/NavbarComponent';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 function App() {
 
@@ -12,8 +15,22 @@ function App() {
 
   return (
     <>
-      <LoginComponent authUser={authUser} setAuthUser={setAuthUser} />
-      <HomeComponent username={authUser?.username} />
+      <Router>
+
+        <AppBar color="primary" position="static">
+          <Toolbar>
+              <Typography variant="h5" color="inherit">
+                <NavbarComponent authUser={authUser}/>
+              </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route path="/home" render={() => <HomeComponent username={authUser?.username} /> } />
+          <Route path="/login" render={() => <LoginComponent authUser={authUser} setAuthUser={setAuthUser} /> } />
+        </Switch>
+        
+      </Router>
     </>
   );
 }
--- poc-demos/react/poc-react-ts/src/remote/auth-service.ts
+++ poc-demos/react/poc-react-ts/src/remote/auth-service.ts
@@ -4,3 +4,7 @@ export async function authenticate(username: string, password: string) {
     let response = await revaboardsClient.post('/auth', {username, password});
     return await response.data;
 }
+export async function register(username: string, password: string) {
+    let response = await revaboardsClient.post('/register', {username, password});
+    return await response.data;
+}
