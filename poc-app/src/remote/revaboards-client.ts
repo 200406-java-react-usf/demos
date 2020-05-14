0a613553aa0979b057f9e203a01507e491a5fbb1
jdvalencia <jdvalencia@users.noreply.github.com>
2020-05-14 15:38:02 -0400

    withCredentials added to axios create

--- poc-app/src/remote/revaboards-client.ts
+++ poc-app/src/remote/revaboards-client.ts
@@ -4,5 +4,6 @@ export const revaboardsClient = axios.create({
     baseURL: 'http://p1reimbursement-env.eba-2gpgjpzh.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
+    withCredentials: true
 });
