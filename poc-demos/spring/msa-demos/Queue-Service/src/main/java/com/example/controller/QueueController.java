eba8497e6374bc6140065b8a5a565a91f5b9771b
connellrobert <robert.lel.connell@gmail.com>
2020-06-08 17:40:40 -0400

    adding mq and dockerfile

---
+++ poc-demos/spring/msa-demos/Queue-Service/src/main/java/com/example/controller/QueueController.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.QueueService;
+
+@RestController
+@RequestMapping("/message")
+public class QueueController {
+
+	@Autowired
+	private QueueService qs;
+	
+	@GetMapping
+	public String getMessage() {
+		return qs.getMessage();
+	}
+	
+	@PostMapping
+	public String sendMessage(@RequestBody String message) {
+		return qs.sendMessage(message);
+	}
+	
+	
+}
